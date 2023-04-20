4c9bc9147a9432bafdbad70740e33c7ab8d2c2e1
Amit Karpe <amitkarpe@gmail.com>
2023-04-20 11:42:49 +0000

    Tested for EVM.

---
+++ evm/main/config.py
@@ -0,0 +1,5 @@
+# config.py
+RPC_URL = "https://polygon-mainnet.infura.io/v3/d162e1d2d54e4fd5b07a78b9b9176728"
+BUCKET_NAME = "project-ubuntu-gnosis-2023"
+SECRET_KEY_FILE = "secret.key"
+AWS_PROFILE_NAME = "default"
